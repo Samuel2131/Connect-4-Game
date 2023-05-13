f42c03234bab94b56c3ca97dc46be26c5afcc484
Samuel2131 <samperisi.samuel@gmail.com>
2023-05-13 17:17:30 +0200

    Game completed

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -6,10 +6,10 @@ import vuoto from "../img/vuoto.png";
 import { useEffect, useState } from "react";
 
 const DIM = 8; 
-//Todo: add check win for col and diagonal, change result win, add random turn;
-export const GameBoard = (props: any) => {
+
+export const GameBoard = () => {
     const [board, setBoard] = useState<string[][] | null>(null);
-    const [turn, setTurn] = useState<1 | 2>(1);
+    const [turn, setTurn] = useState<1 | 2>(Math.random() > 0.5 ? 1 : 2);
     const [win, setWin] = useState<0 | 1 | 2 | 3>(0);
 
     const initBoard = () => {
@@ -19,6 +19,7 @@ export const GameBoard = (props: any) => {
     }
 
     const addDot = (row: number, col: number) => {
+        if(win !== 0) return;
         if(col === -1) alert("Full column...");
         else if(board && board[row][col] === vuoto) {
             board[row][col] = turn === 1 ? rosso : giallo;
@@ -29,6 +30,8 @@ export const GameBoard = (props: any) => {
 
     const reset = () => {
         initBoard();
+        setTurn(Math.random() > 0.5 ? 1 : 2);
+        setWin(0);
     }
 
     const checkWin = () => {
@@ -37,25 +40,40 @@ export const GameBoard = (props: any) => {
             for(let col=0;col<DIM;col++){
                 if(board[row][col] !== vuoto){
                     //Row:
-                    const tempArr = [];
+                    let tempArr = [];
                     for(let i=row+1;i<=row+3;i++){
                         try{
                             if((board[i][col] === board[row][col])) tempArr.push(board[i][col]);
                             else break;
                         } catch(e){};
                     }
-                    if(tempArr.length === 3) board[row][col] === rosso ? alert("Player 1 win") : alert("Player 2 win");
-                    
-                    for(let i=row-1;i<=row-3;i--){
+                    if(tempArr.length === 3) board[row][col] === rosso ? setWin(1) : setWin(2);
+
+                    //Col:
+                    tempArr = [];
+                    for(let i=col+1;i<=col+3;i++){
                         try{
-                            if(board[i][col] && (board[i][col] === board[row][col])) tempArr.push(board[i][col]);
+                            if((board[row][i] === board[row][col])) tempArr.push(board[row][i]);
                             else break;
                         } catch(e){};
                     }
-                    if(tempArr.length === 3) board[row][col] === rosso ? alert("Player 1 win") : alert("Player 2 win");
+                    if(tempArr.length === 3) board[row][col] === rosso ? setWin(1) : setWin(2);
+
+                    //Diagonal:
+                    let d1 = [], d2 = [];
+                    for(let i=1;i<=3;i++){
+                        try{
+                            if(board[row+i][col+i] && (board[row+i][col+i] === board[row][col])) d1.push(board[row+i][col+i]);
+                        }catch(e){};
+                        try{
+                            if(board[row-i][col+i] && (board[row-i][col+i] === board[row][col])) d2.push(board[row-i][col+i]);
+                        }catch(e){};
+                    }
+                    if(d1.length === 3 || d2.length === 3) board[row][col] === rosso ? setWin(1) : setWin(2);
                 }
             }
         }
+        if(board.flat(2).every((img) => img !== vuoto)) setWin(3);
     }
 
     useEffect(() => {
@@ -68,14 +86,22 @@ export const GameBoard = (props: any) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [board]);
 
+    useEffect(() => {
+        if(win !== 0) {
+            setTimeout(() => reset(), 3000);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [win]);
+
     return(
         <>
             <h1 className="fw-bold">Force 4 game</h1>
             <p className="fs-3">Player 1 use <span className="redDot">red</span> dot, Player 2 use <span className="yellowDot">yellow</span> dot</p>
             <p className="fs-3">Player {turn} it's your turn</p>
+            {win === 1 || win === 2 ? <p className="fs-3">Player {win} has won!</p> : win === 3 ? <p className="fs-3">Tie...</p> : null}
             <div className="game-board">
                 {React.Children.toArray(board?.map((arr, index) => {
-                    return <div onClick={() => addDot(index, DIM-1)}>{React.Children.toArray(arr.map((img) => <div className="box" ><img src={img} width={"100%"} height={"100%"} alt="..." /></div>))}</div>
+                    return <div onClick={() => addDot(index/*col*/, DIM-1/*row*/)}>{React.Children.toArray(arr.map((img) => <div className="box" ><img src={img} width={"100%"} height={"100%"} alt="..." /></div>))}</div>
                 }))}
             </div>
             <button type="button" className="btn btn-danger mt-3" onClick={() => reset()}>Reset</button>
